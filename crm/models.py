9cfda602fe5a187dbcbc083e18a4cee467b60f23
iHusk <tjhayes@unomaha.edu>
2021-10-05 22:14:32 -0500

    Added CRUD for Customers, Products, and Services.

--- crm/models.py
+++ crm/models.py
@@ -1,6 +1,5 @@
-from django.utils import timezone
-
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
@@ -16,6 +15,56 @@ class Customer(models.Model):
     state = models.CharField(max_length=50)
     zipcode = models.CharField(max_length=10)
     phone_number = models.CharField(max_length=50)
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+    def created(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def updated(self):
+        self.updated_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return str(self.cust_name)
+
+
+class Service(models.Model):
+    cust_name = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='services')
+    service_category = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=200)
+    setup_time = models.DateTimeField(
+        default=timezone.now)
+    cleanup_time = models.DateTimeField(
+        default=timezone.now)
+    service_charge = models.DecimalField(max_digits=10, decimal_places=2)
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+    def created(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def updated(self):
+        self.updated_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return str(self.cust_name)
+
+
+class Product(models.Model):
+    cust_name = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='products')
+    product = models.CharField(max_length=100)
+    p_description = models.TextField()
+    quantity = models.IntegerField()
+    pickup_time = models.DateTimeField(
+        default=timezone.now)
+    charge = models.DecimalField(max_digits=10, decimal_places=2)
     created_date = models.DateTimeField(
         default=timezone.now)
     updated_date = models.DateTimeField(auto_now_add=True)
