33a9af6d4987ea3daa9f6c5633cf0a6647974c85
iHusk <tjhayes@unomaha.edu>
2021-10-07 21:11:00 -0500

    Export CSV working. requirements.txt created. updated views and html.

---
+++ crm/utils.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+
+
+def render_to_pdf(template_src, pdf_name, context_dict={}):
+    template = get_template(template_src)
+    html = template.render(context_dict)
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename="{pdf_name}.pdf'
+    pdf_status = pisa.CreatePDF(html, dest=response)
+
+    if pdf_status.err:
+        return HttpResponse('Some errors were encountered <pre>' + html + '</pre>')
+
+    return response
+
